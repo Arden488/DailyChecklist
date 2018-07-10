31dc9efeff2e6c7458e0a3351e12fccb18341763
Anton Samoilov <absamoilov88@gmail.com>
2018-07-10 23:38:36 +0300

    Add and connect form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppForm from './components/AppForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppForm
   }
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
