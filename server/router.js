c6430cecd1e96855e0c59e0fb58d632a0c42091f
Anton Samoilov <absamoilov88@gmail.com>
2018-07-11 14:46:29 +0300

    Add questions dummy route

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 const Auth = require('./controllers/auth');
+const Question = require('./controllers/questions');
 const passportService = require('./services/passport');
 const passport = require('passport');
 
@@ -6,9 +7,16 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false })
 
 module.exports = (app) => {
-  app.get('/', requireAuth, (req, res) => {
-    res.send({ msg: 'Test' });
+  // app.get('/', requireAuth, (req, res) => {
+  //   res.send({ test: 'test' })
+  // })
+  app.get('/questions', (req, res) => {
+    res.send([{
+      title: 'abc',
+      type: 'boolean'
+    }])
   });
+  app.post('/addQuestion', requireAuth, Question.addQuestion);
   app.post('/signin', requireSignin, Auth.signin);
   app.post('/signup', Auth.signup);
 }
