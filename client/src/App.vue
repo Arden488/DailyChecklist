5193114ba163b8149d21f5b01f16e836fb27f7ec
Anton Samoilov <absamoilov88@gmail.com>
2018-07-12 17:07:44 +0300

    Refactor general layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
-    <MainNavigation />
     <el-container>
-      <el-main>
-        <router-view></router-view>
-      </el-main>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <MainNavigation />
+      </el-aside>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px; background-color: rgb(238, 241, 246); line-height: 60px;">
+          <el-dropdown>
+            <el-button size="mini" icon="el-icon-setting">User</el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>Profile</el-dropdown-item>
+              <el-dropdown-item>Exit</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
