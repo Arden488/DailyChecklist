9db3dc319a65fd5c87575e03c0b6385c2a31fa9b
Anton Samoilov <absamoilov88@gmail.com>
2018-07-12 17:11:34 +0300

    Add create question api method

--- client/src/services/QuestionsService.js
+++ client/src/services/QuestionsService.js
@@ -1,6 +1,9 @@
 import api from '@/services/api';
 
 export default {
+  createQuestion (params) {
+    return api().post('questions', params);
+  },
   fetchQuestions () {
     return api().get('questions');
   }
