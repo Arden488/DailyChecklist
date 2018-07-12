bf5bda70779dbc7f485ce835d45ce7494a35273b
Anton Samoilov <absamoilov88@gmail.com>
2018-07-12 17:12:22 +0300

    Hardcode test token and add header for axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjQ1MjhjNzBjYTMwNjEyMjcwN2Q5MDMifQ.lzEogpzuXLUqhPfEJHmjsUxK5MhU5BzH5s4WouunBlI';
+
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3030'
+    baseURL: 'http://localhost:3030',
+    headers: { 'Authorization': "bearer " + token }
   })
 }
