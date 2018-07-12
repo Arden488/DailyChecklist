31201a778754b5cb57f5d8f0b8681d5740024cb2
Anton Samoilov <absamoilov88@gmail.com>
2018-07-12 17:10:20 +0300

    Add table to show questions list

--- client/src/components/pages/QuestionsPage.vue
+++ client/src/components/pages/QuestionsPage.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>{{questions}}</div>
+  <div>
+    <h1>List of questions</h1>
+    <el-table
+      :data="questions"
+      :default-sort = "{prop: '_id', order: 'descending'}"
+      style="width: 100%">
+      <el-table-column
+        label="Title"
+        prop="title"
+        sortable
+      >
+      </el-table-column>
+      <el-table-column
+        label="Type"
+        prop="type"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        label="Operations"
+        width="250"
+      >
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="ID"
+        prop="_id"
+        sortable
+        width="220">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -13,9 +52,17 @@ export default {
     }
   },
   methods: {
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+
     async getQuestions () {
       const response = await QuestionsService.fetchQuestions()
-      this.questions = response
+      this.questions = response.data.items
     }
   },
   mounted() {
