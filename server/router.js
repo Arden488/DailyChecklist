4a028b292567ac531e3f2dbc2f242eb7a7de0884
Anton Samoilov <absamoilov88@gmail.com>
2018-07-13 12:29:18 +0300

    Update server routes

--- server/router.js
+++ server/router.js
@@ -7,9 +7,9 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false })
 
 module.exports = (app) => {
-  // app.get('/', requireAuth, (req, res) => {
-  //   res.send({ test: 'test' })
-  // })
+  app.delete('/questions/:delete', Question.deleteQuestion);
+  app.put('/questions/:id', Question.updateQuestion);
+  app.get('/questions/:id', Question.getQuestion);
   app.get('/questions', Question.getQuestions);
   app.post('/questions', requireAuth, Question.addQuestion);
   app.post('/signin', requireSignin, Auth.signin);
