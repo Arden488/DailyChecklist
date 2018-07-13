d15a0cc2cd16500226935736c13d00f376449af3
Anton Samoilov <absamoilov88@gmail.com>
2018-07-13 12:28:12 +0300

    Add update delete and get one methods for api

--- client/src/services/QuestionsService.js
+++ client/src/services/QuestionsService.js
@@ -1,6 +1,15 @@
 import api from '@/services/api';
 
 export default {
+  fetchQuestion (id) {
+    return api().get(`questions/${id}`);
+  },
+  deleteQuestion (id) {
+    return api().delete(`questions/${id}`);
+  },
+  updateQuestion (params) {
+    return api().put(`questions/${params.id}`, params)
+  },
   createQuestion (params) {
     return api().post('questions', params);
   },
