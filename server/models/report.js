70ec3b387a6914da810fb70c2e8939e918c73024
Anton Samoilov <absamoilov88@gmail.com>
2018-07-13 13:47:17 +0300

    Add report model

---
+++ server/models/report.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reportSchema = new Schema({
+  date: Date,
+  answers: [{
+    label: String,
+    fieldType: String,
+    value: String,
+    options: {
+      range: String,
+      reverse: Boolean,
+      badEndsOn: String,
+      goodStartsOn: String,
+      values: [{
+        key: Number,
+        label: String,
+        value: Number
+      }]
+    }
+  }]
+});
+
+const ModelClass = mongoose.model('report', reportSchema);
+
+module.exports = ModelClass;
