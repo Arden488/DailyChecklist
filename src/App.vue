8d719b43339b379e06a18ad933017563640b0c23
Anton Samoilov <absamoilov88@gmail.com>
2018-07-16 23:35:24 +0300

    Move layout to layouts

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <MainNavigation />
-      </el-aside>
-      <el-container>
-        <el-header style="text-align: right; font-size: 12px; background-color: rgb(238, 241, 246); line-height: 60px;">
-          <el-dropdown>
-            <el-button size="mini" icon="el-icon-setting">User</el-button>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>Profile</el-dropdown-item>
-              <el-dropdown-item>Exit</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </el-header>
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-      </el-container>
-    </el-container>
+    <component :is="layout">
+      <router-view></router-view>
+    </component>
   </div>
 </template>
 
 <script>
-import MainNavigation from './components/MainNavigation.vue'
+const defaultLayout = 'default';
 
 export default {
   name: 'app',
-  components: {
-    MainNavigation
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || defaultLayout) + '-layout';
+    }
   }
 }
 </script>
