7c8cf16d310fb1f2efe29a222ed0b4a4e3a1e769
Anton Samoilov <absamoilov88@gmail.com>
2018-07-16 23:37:49 +0300

    Add sign in page

---
+++ src/components/pages/SignInPage.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+
+    <div class="login-form">
+      <h2 style="text-align: center">Sign In to Daily Checklist</h2>
+
+      <el-card shadow="never">
+        <el-form :model="loginForm" :rules="rules" ref="loginForm">
+          <el-form-item prop="login">
+            <el-input v-model="loginForm.login" placeholder="Enter your email"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" v-model="loginForm.password" placeholder="Enter your password"></el-input>
+          </el-form-item>
+          <div style="text-align: center">
+            <el-button type="primary" @click="submitForm()" style="padding-left: 50px; padding-right: 50px;">Sign in</el-button>
+          </div>
+        </el-form>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService';
+
+export default {
+  name: 'SignInPage',
+  data () {
+    return {
+      loginForm: {
+        login: '',
+        password: ''
+      },
+      rules: {
+        login: [
+          { required: true, message: 'Field \'Email\' is required', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'Field \'Password\' is required', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$refs['loginForm'].validate((valid) => {
+        if (valid) {
+          const data = {
+            email: this.loginForm.login,
+            password: this.loginForm.password,
+          }
+          this.signIn(data);
+        }
+      })
+    },
+    async signIn(data) {
+      const response = await AuthService.signIn(data);
+      if (await response.status === 200) {
+        localStorage.setItem('access_token', response.data.token);
+        this.$router.push({ name: 'Overview' });
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.login-form {
+  width: 400px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+
