c6b02fe15cf5e18dec7ffdae98fdf512de32b913
Anton Samoilov <absamoilov88@gmail.com>
2018-07-16 23:37:06 +0300

    Add sign in and meta for each item

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,32 +3,56 @@ import CreateReport from '@/components/pages/CreateReportPage.vue';
 import Overview from '@/components/pages/OverviewPage.vue';
 import CreateQuestion from '@/components/pages/CreateQuestionPage.vue';
 import EditQuestion from '@/components/pages/EditQuestionPage.vue';
+import SignInPage from '@/components/pages/SignInPage.vue'
 
 const routes = [
   {
+    path: '/signin',
+    name: 'Sign In',
+    component: SignInPage,
+    meta: {
+      layout: 'clean'
+    }
+  },
+  {
     path: '/',
     name: 'Overview',
-    component: Overview
+    component: Overview,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/questions',
     name: 'Questions',
-    component: Questions
+    component: Questions,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/questions/create',
     name: 'CreateQuestion',
-    component: CreateQuestion
+    component: CreateQuestion,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/questions/:id',
     name: 'EditQuestion',
-    component: EditQuestion
+    component: EditQuestion,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/reports/create',
     name: 'CreateReport',
-    component: CreateReport
+    component: CreateReport,
+    meta: {
+      requiresAuth: true
+    }
   }
 ];
 
