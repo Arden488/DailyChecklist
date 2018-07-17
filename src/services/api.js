24bd14da106d72aad0d70e284aefeedc1af735e5
Anton Samoilov <absamoilov88@gmail.com>
2018-07-17 22:01:55 +0300

    Move token check down to export

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { SERVER_URL } from '../config';
 
-const token = localStorage.getItem('access_token');
-
 export default () => {
-  return axios.create({
+  const token = localStorage.getItem('access_token');
+  const instance = axios.create({
     baseURL: SERVER_URL[process.env.NODE_ENV],
     headers: token ? { 'Authorization': "bearer " + token } : {}
-  })
+  });
+
+  return instance;
 }
