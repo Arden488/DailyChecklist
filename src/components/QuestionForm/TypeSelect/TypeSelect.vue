af4375e9620c9744ba0c4d9532848aa0bcabf4cf
Anton Samoilov <absamoilov88@gmail.com>
2018-07-21 14:34:17 +0300

    Move form part to separate component

---
+++ src/components/QuestionForm/TypeSelect/TypeSelect.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-select v-model="typeLocal" @change="updateType($event)" placeholder="Select one">
+    <el-option label="Time picker" value="time"></el-option>
+    <el-option label="Yes/No" value="boolean"></el-option>
+    <el-option label="Select variant" value="select"></el-option>
+    <el-option label="Mood rating" value="mood"></el-option>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: 'TypeSelect',
+  props: [ 'type' ],
+  data () {
+    return {
+      typeLocal: this.type,
+    }
+  },
+  methods: {
+    updateType: function (value) {
+      // console.log(value);
+      this.$emit('type', value);
+    }
+  }
+}
+</script>
