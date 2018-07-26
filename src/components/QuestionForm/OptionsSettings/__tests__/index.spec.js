de1f9e9c31acac0e895b0489540c039f65398983
Anton Samoilov <absamoilov88@gmail.com>
2018-07-26 21:22:47 +0300

    Work in progress: tests for OptionsSettings

--- src/components/QuestionForm/OptionsSettings/__tests__/index.spec.js
+++ src/components/QuestionForm/OptionsSettings/__tests__/index.spec.js
@@ -1,27 +1,58 @@
-/* global it, describe, expect */
+/* global it, describe, expect, beforeAll */
 
-import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import { shallowMount, mount, createLocalVue, config } from '@vue/test-utils';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import OptionsSettings from '../OptionsSettings.vue';
 
+let wrapper;
 const localVue = createLocalVue();
+localVue.use(ElementUI);
 localVue.use(VueRouter);
 
-describe('OptionsSettings layout', () => {
+// config.provide['elForm'] = { $options: {} }
+
+beforeAll(() => {
+  wrapper = mount(OptionsSettings, { 
+    localVue
+  });
+});
+
+describe('OptionsSettings component', () => {
   it('is a Vue instance', () => {
-    localVue.use(ElementUI);
-    const wrapper = shallowMount(OptionsSettings, { 
-      localVue
-    });
     expect(wrapper.isVueInstance).toBeTruthy()
   })
 
   it('is renders correctly', () => {
-    const wrapper = shallowMount(OptionsSettings, { 
-      localVue
+    wrapper.setProps({
+      type: 'mood',
+      options: {
+        rangeStart: 0,
+        rangeEnd: 10,
+        goodStartsOn: 5,
+        badEndsOn: 3,
+      } 
     });
     expect(wrapper).toMatchSnapshot()
   })
+
+  // it('it\'s setting type correctly', () => {
+  //   wrapper.setData({ typeLocal: 'boolean' })
+
+  //   expect(wrapper.vm.typeLocal).toBe('boolean');
+
+  //   const option = wrapper.find('.el-select-dropdown__item');
+  //   option.trigger('click');
+
+  //   expect(wrapper.vm.typeLocal).toBe('time');
+  // })
+
+  // it('emits event on change', () => {
+  //   wrapper.vm.$emit('type', 'time');
+
+  //   expect(wrapper.emitted().type).toBeTruthy();
+  //   expect(wrapper.emitted().type.length).toBe(1);
+  //   expect(wrapper.emitted().type[0]).toEqual(['time']);
+  // })
 });
