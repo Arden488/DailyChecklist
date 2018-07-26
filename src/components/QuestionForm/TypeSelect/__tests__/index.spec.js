552240faf888032af5a17a2f4d90d5f596dd455e
Anton Samoilov <absamoilov88@gmail.com>
2018-07-26 20:18:20 +0300

    Refactored tests for TypeSelect component

--- src/components/QuestionForm/TypeSelect/__tests__/index.spec.js
+++ src/components/QuestionForm/TypeSelect/__tests__/index.spec.js
@@ -6,24 +6,40 @@ import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import TypeSelect from '../TypeSelect.vue';
 
+let wrapper;
 const localVue = createLocalVue();
 localVue.use(ElementUI);
 localVue.use(VueRouter);
 
+beforeAll(() => {
+  wrapper = shallowMount(TypeSelect, { localVue });
+});
+
 describe('Default layout', () => {
   it('is a Vue instance', () => {
-    const wrapper = shallowMount(TypeSelect, { localVue });
     expect(wrapper.isVueInstance).toBeTruthy()
   })
 
   it('is renders correctly', () => {
-    const wrapper = shallowMount(TypeSelect, { localVue });
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('it\'s type is empty', () => {
-    const wrapper = shallowMount(TypeSelect, { localVue });
-    console.log(wrapper.vm.props);
-    expect(wrapper.vm.typeLocal).toBe('');
+  it('it\'s setting type correctly', () => {
+    wrapper.setData({ typeLocal: 'boolean' })
+
+    expect(wrapper.vm.typeLocal).toBe('boolean');
+
+    const option = wrapper.find('.el-select-dropdown__item');
+    option.trigger('click');
+
+    expect(wrapper.vm.typeLocal).toBe('time');
+  })
+
+  it('emits event on change', () => {
+    wrapper.vm.$emit('type', 'time');
+
+    expect(wrapper.emitted().type).toBeTruthy();
+    expect(wrapper.emitted().type.length).toBe(1);
+    expect(wrapper.emitted().type[0]).toEqual(['time']);
   })
 });
