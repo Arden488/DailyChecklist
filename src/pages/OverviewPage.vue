cfeb4b1f3ad8505500904918eb3d9a2417d01493
Anton Samoilov <absamoilov88@gmail.com>
2018-07-29 01:27:28 +0300

    Add value preprocessing

--- src/pages/OverviewPage.vue
+++ src/pages/OverviewPage.vue
@@ -35,8 +35,23 @@ export default {
       return returnOption;
     },
 
+    preprocessFieldValue(field) {
+      if(field.fieldType === 'time' || field.type === 'time') field.value = this.translateIntToTime(field.value);
+      if(field.fieldType === 'mood' || field.type === 'mood') field.value = parseInt(field.value);
+      if(field.fieldType === 'boolean' || field.type === 'boolean') field.value = !!(field.value == 'true');
+
+      return field.value;
+    },
+
+    translateIntToTime(int) {
+      let s = int + '';
+      s = s.length === 1 ? '0' + s : s;
+      return `${s}:00`;
+    },
+
     prepareAnswers(item, answers) {
       answers.forEach((a, i) => {
+        a.value = this.preprocessFieldValue(a);
         if (a.fieldType === 'select')
           answers[i].selectedOption = this.getOptionData(a);
       })
